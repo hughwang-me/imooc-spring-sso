cb4eedcd9823dca27bfe2f5e0ef64eb9a15876ce
王欢 <18501667737@163.com>
2021-06-06 19:57:39 +0800

    init

---
+++ src/main/java/com/uwjx/imooc/ImoocSpringSsoApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocSpringSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocSpringSsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uwjx/imooc/ImoocSpringSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.imooc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocSpringSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
